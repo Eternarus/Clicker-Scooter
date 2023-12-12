33bcf43b2a259fe0095818da587a08da6f49969c
Eternatus <120200880+Eternarus@users.noreply.github.com>
2023-12-12 22:01:28 +0500

    fix

--- src/App.js
+++ src/App.js
@@ -48,7 +48,7 @@ function App() {
         ...prevState,
         x: prevState.x + n,
         [k]: prevState[k] + 1,
-        number: prevState.number - n,
+        number: prevState.number - z,
         [cost]: prevState[cost] * 1.5
       }));
     };
